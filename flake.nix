7ca5b3ba3c15302892c860c72e1cd855cec34d60
Will Cohen <willcohe@umich.edu>
2025-10-08 22:43:46 -0400

    add arm builder back in. Ugly...

--- flake.nix
+++ flake.nix
@@ -72,6 +72,12 @@
                         }];  
 
     callPackage = set: f: overrides: f ((builtins.intersectAttrs (builtins.functionArgs f) set) // overrides); 
+    pkgs = import nixpkgs { system = "x86_64-linux"; };
+    pkgsAarch64 = import nixpkgs { system = "aarch64-linux"; };
+
+    iso = (pkgsAarch64.nixos {
+      imports = [ "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-base.nix" ];
+    }).config.system.build.isoImage;
     in {
         # inherit localModules lib;
         testInputs = flakeInputs;  
@@ -106,7 +112,25 @@
         }; 
         
         vms = {
-            arm-builder = self.legacyPackages.x86_64-linux.arm-builder.default; 
+            arm-builder = pkgs.writeScriptBin "run-nixos-vm" ''
+                #!${pkgs.runtimeShell}
+                if [ -z $1 ]; then
+                    echo "Please supply the qcow file for the vm" 
+                    exit
+                fi
+
+                ${pkgs.qemu}/bin/qemu-system-aarch64 \
+                    -machine virt,gic-version=max \
+                    -cpu max \
+                    -m 4G \
+                    -smp 4 \
+                    -drive file="/home/nixos/arm-builder.qcow2",if=virtio,format=qcow2 \
+                    -drive file=$(echo ${iso}/iso/*.iso),format=raw,readonly=on \
+                    -nographic \
+                    -bios ${pkgsAarch64.OVMF.fd}/FV/QEMU_EFI.fd \
+                    -net nic,model=virtio \
+                    -net user,hostfwd=tcp::2222-:22
+                '';
         };
         
         

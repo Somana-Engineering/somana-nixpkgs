e476b27e4d0f16fa3a3a3fb55cc80a4e1bd74b73
Luke Wing <lukewing@Lukes-MacBook-Pro.local>
2025-11-14 16:03:46 -0800

    woot

--- overlays/sprinter-agent/default.nix
+++ overlays/sprinter-agent/default.nix
@@ -13,10 +13,10 @@ buildGoModule (finalAttrs: {
     owner = "somana-engineering";
     repo = "sprinter-agent";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-H+YI+1VcuvGczfiVz5iy9N9gg2SeV+xjKmPM0V6HB6U=";
+    hash = "";
   };
 
-  vendorHash = "sha256-PnG0q/97EnM4leyZ7X6Jswd81l9elz8GTr6B42qRlcM=";
+  vendorHash = "";
 
   ldflags = [
     "-s"

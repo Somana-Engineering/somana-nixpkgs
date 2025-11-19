c5442d71f35d5cb4942f4d1062c05169071d72f7
Luke Wing <lukewing@Mac.localdomain>
2025-11-19 13:46:48 -0500

    rid

--- overlays/sprinter-agent/default.nix
+++ overlays/sprinter-agent/default.nix
@@ -13,7 +13,7 @@ buildGoModule (finalAttrs: {
     owner = "somana-engineering";
     repo = "sprinter-agent";
     tag = "v${finalAttrs.version}";
-    hash = "";
+    hash = "sha256-CnVFJokFSPlaTbu/L0506PdGS1kRkxncLuwwpXhdYjs=";
   };
 
   vendorHash = "";

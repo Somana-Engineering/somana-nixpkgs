8f2cd40402fd5c5137c44cf56275cfbbb222dd8e
Luke Wing <lukewing@Mac.localdomain>
2025-11-19 13:30:37 -0500

    has

--- overlays/sprinter-agent/default.nix
+++ overlays/sprinter-agent/default.nix
@@ -16,7 +16,7 @@ buildGoModule (finalAttrs: {
     hash = "sha256-qRkHkZjUdg8sX+cU9FJP6jI/G00K58VFACMo33tRK/w=";
   };
 
-  vendorHash = "sha256-PnG0q/97EnM4leyZ7X6Jswd81l9elz8GTr6B42qRlcM=";
+  vendorHash = "sha256-YLBgTvkm33tLWwlYpB+ShW6XHcW/MqIquXlO6FIgiEU=";
 
   ldflags = [
     "-s"

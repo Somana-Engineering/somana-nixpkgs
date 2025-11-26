6fa79689be67bf67fc1670e5a591428f1ae27379
Will <will@somana.tech>
2025-11-26 07:18:44 -0500

    pi5 build

--- flake.nix
+++ flake.nix
@@ -6,6 +6,16 @@
     jetpack-nixos.url = "github:anduril/jetpack-nixos";
     nixpkgs.url = "github:NixOS/nixpkgs/release-25.05";
     nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay";
+    nixos-raspberrypi.url = "github:nvmd/nixos-raspberrypi"; 
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nixos-raspberrypi.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+    ];
   };
 
   outputs = inputs: {
